579c775806a50edc57ac85bdd3aecaa90584fec1
Matias <mantitaaa89@gmail.com>
2023-06-13 17:58:09 -0300

    feat: ui wrapper

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,14 +1,20 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-full bg-gradient-to-r from-sky-500 to-indigo-500 ">
-        <nav className="p-4 border-black flex justify-end fixed w-full">
-          <ConnectButton></ConnectButton>
-        </nav>
-        <main className="h-full flex items-center justify-center">
-          <h1 className="font-mono text-5xl">Cool UI Here</h1>
-        </main>
+      <div className="flex flex-col min-h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
+        <Header />
+        <div className="w-full min-h-[70vh] p-4 flex justify-between space-x-3">
+          <div className="border-2 flex-auto max-w-[30%] p-2">
+            all read data
+          </div>
+          <div className="border-2 flex-1 flex flex-col space-y-2 p-2">
+            <div className="border-2 flex-1">deployer write functions</div>
+            <div className="border-2 flex-1">participant write functions</div>
+          </div>
+        </div>
+        <Footer />
       </div>
     </>
   );
